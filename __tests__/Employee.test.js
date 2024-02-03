e4ebe770d1712f570a9c336b6a0cfe14439151cb
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-02-02 19:53:10 -0800

    Added psuedocode for Employee.test.js

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,14 @@
+// Employee is a Parent Class that will contain properties and results from methods
+
+// Test if the employee has a
+    // name
+    // id
+    // email
+
+// Test method for getting employee name. 
+
+// Test method for getting employee id
+
+// Test method for getting employee email
+
+// Test method for getRole (return Employee
