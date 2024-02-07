7640f4ced949a7465758115bf491dc3694b94926
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-02-06 23:53:00 -0800

    Added tests to Manager.test.js, currently failing but ready to be modified

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,3 +1,11 @@
+// Test the manager project
+test('create the manager object', () => {
+
+});
+
 // Check for property of officeNumber
+test('test for the managers office number', () => {
+
+});
 
 // Run test for getRole and return Manager
