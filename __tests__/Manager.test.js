3bc7acc56c48b39f2c864a26795817c62adac0b7
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-02-07 12:55:55 -0800

    Applied modifications to Manager.test.js, currently the manager object is failing. However modifications have been made to getRole() in efforts to make it more dynamic. It's passed as a second parameter within the Employee class constructor.

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,11 +1,23 @@
+const Employee = require('../lib/Employee');
+
 // Test the manager project
 test('create the manager object', () => {
-
+    const employee = new Employee('Test');
+    expect(employee.name).toBe('Test');
+    // Currently failing
+    expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 // Check for property of officeNumber
 test('test for the managers office number', () => {
-
+    const officeNumber = Math.floor(Math.random());
+    expect(officeNumber).toEqual(expect.any(Number));
 });
 
 // Run test for getRole and return Manager
+test('get employee role and return the Manager role', () => {
+    const employee = new Employee('Test', 'Manager');
+
+    employee.getRole('Manager');
+    expect(employee.role).toBe('Manager');
+});
