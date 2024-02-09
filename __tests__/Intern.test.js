d281d659984ddf22ca6e583b4217585a668e19d3
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-02-08 23:15:11 -0800

    Adjusted the intern test further, the main object is passing now.

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,7 +1,23 @@
-// Extends the Employee class
+const Employee = require('../lib/Employee');
+const Intern = require('../lib/Intern');
 
-// Check for property of school
+// Test the intern object
+test('create the intern object', () => {
+    const employee = new Employee('Test', 'Intern');
+    const intern = new Intern();
+
+    expect(employee.name).toBe('Test');
+    expect(employee.role).toBe('Intern');
+
+    // Check for property of school
+})
 
 // Test for getSchool() method
+// test('Test for the interns school', () => {
+
+// });
 
 // Test for getRole and return 'intern'
+// test('get employee role and return the Intern position', () => {
+
+// });
