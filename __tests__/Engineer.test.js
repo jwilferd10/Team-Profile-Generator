56c94e970ce1f4c15b40321102f3d10f23818410
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-02-08 23:59:23 -0800

    Finished all tests within Engineer.test.js, all tests have followed TDD rules.

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,5 +1,25 @@
-// Test for property of github 
+const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
 
-// Run test for getGithub()
+// Test for engineer object 
+test('create and test engineer object', () => {
+    const employee = new Employee('Test');
+    const engineer = new Engineer();
+
+    expect(employee.name).toEqual('Test');
+});
+
+// Test for property of github 
+test('test getGitHub to see if method works', () => {
+    const engineer = new Engineer();
+
+    const userGitHub =  engineer.getGitHub();
+    expect(userGitHub).toEqual('testGitHub');
+});
 
 // run test for getRole() and return engineer
+test('test getRole to return engineer title', () => {
+    const employee = new Employee('', 'Engineer');
+
+    expect(employee.role).toEqual('Engineer');
+});
