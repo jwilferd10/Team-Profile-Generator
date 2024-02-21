b209fc6b76722d2edf2ca4fe589963380010700f
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-02-20 20:43:12 -0800

    Updated index.js to utilize asynchronus operations with async/await

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const generateTeam = require('./lib/Generator');
 
-// Invoke the application
-new generateTeam().promptIntro();
+const startGenerator = async () => {
+    try {
+        // Invoke the application
+        await generateTeam().promptIntro();
+
+        const profileHTML = generatePage(generatedProfile);
+
+        // Generate the appropriate files
+        await writeHTMLFile(profileHTML);
+        
+        // TODO: Include code that writes CSS file
+
+        // Success Log
+        console.log('Team Profile has been successfully created.');
+    } catch (error) {
+        console.error(`An error has occurred: ${error.message}`);
+    };
+};
+
+startGenerator();
