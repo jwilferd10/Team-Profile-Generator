0d51335ce6be8751b899b9e5b43eb2bc71ab0141
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-02-20 20:15:19 -0800

    Added psuedo-code to generate-html file

--- src/generate-html.js
+++ src/generate-html.js
@@ -1,1 +1,9 @@
 // generate-html will utilize fs to writeFile the HTML file, located inside the /dist directory.
+
+// create a connection to fs
+
+// create a method that will return a promise to return any errors or notify file is created
+
+// TODO: After basic HTML has been implemented, write a CSS file here.
+
+// Export the method
