9036d9b2d7f79eb99c39c3871035b3ffdaeb6017
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-02-20 21:25:46 -0800

    Added psuedocode to the page-template.js

---
+++ src/page-template.js
@@ -0,0 +1,7 @@
+// Create a function titled printEmployees
+
+    // Iterate through each version of employees and print the appropriate html
+
+    // Separate employee's based on role and present them in different sections on the page
+
+// Success log
