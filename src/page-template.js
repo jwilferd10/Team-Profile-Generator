d654bd45089b67ae70c9ca53b245f2ed717f5968
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-02-22 23:43:03 -0800

    Removed a test log and added some psuedocod for a later date.

--- src/page-template.js
+++ src/page-template.js
@@ -3,12 +3,13 @@ const generatePage = (generatedProfile) => {
     const employeeArr = generatedProfile.employeeArr;
 
     console.table(employeeArr);
-    console.log('IM A TEST');
         // Iterate through each version of employees and print the appropriate html
 
         // Separate employee's based on role and present them in different sections on the page
 
         // Use a switch statement to run sub-methods based on the role of an employee
+
+    // Create cards for specific HTML (Ensure code here is DRY)
 
     // Consolidate all the created sections into the main HTML template
 
