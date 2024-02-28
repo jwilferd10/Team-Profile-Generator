45348f4ce719e5930becb9b758c24c6edbf46d9d
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-02-27 18:25:13 -0800

    Added Bootstrap links to the project, will begin testing out code

--- src/page-template.js
+++ src/page-template.js
@@ -121,6 +121,7 @@ const generatePage = (generatedProfile) => {
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
             <title>Generated Team Profile</title>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
             <link rel="stylesheet" href="style.css">
         </head>
     
@@ -138,6 +139,9 @@ const generatePage = (generatedProfile) => {
             <footer>
                 <h3>&copy; ${new Date().getFullYear()} by Jonathan Wilferd</h3>
             </footer>
+
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
         </body>
         </html>
     `;
