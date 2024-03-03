c8276b2a5f32215b2d4d49f1a7787ff796c6abb4
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-03-02 22:05:43 -0800

    Updated engineer.test.js to pass all parameters

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,23 +3,23 @@ const Engineer = require('../lib/Engineer');
 
 // Test for engineer object 
 test('create and test engineer object', () => {
-    const employee = new Employee('Test');
-    const engineer = new Engineer();
+    const engineer = new Engineer('Test', 'Test@example.com', 1, 'Engineer', 'testHub');
 
-    expect(employee.name).toEqual('Test');
+    expect(engineer.name).toBe('Test');
+    expect(engineer.email).toBe('Test@example.com');
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.role).toBe('Engineer');
+    expect(engineer.github).toBe('testHub');
 });
 
 // Test for property of github 
 test('test getGitHub to see if method works', () => {
-    const engineer = new Engineer();
-
-    const userGitHub =  engineer.getGitHub();
-    expect(userGitHub).toEqual('testGitHub');
+    const engineer = new Engineer('Test', 'Test@example.com', 1, 'Engineer', 'testHub');
+    expect(engineer.getGitHub()).toEqual('testHub');
 });
 
 // run test for getRole() and return engineer
 test('test getRole to return engineer title', () => {
-    const employee = new Employee('', 'Engineer');
-
-    expect(employee.role).toEqual('Engineer');
+    const engineer = new Engineer('Test', 'Test@example.com', 1, 'Engineer', 'testHub');
+    expect(engineer.getRole()).toEqual('Engineer');
 });
