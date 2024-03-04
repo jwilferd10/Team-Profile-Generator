e61618166f4eddda7e8740e1ba777407cab9c062
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-03-03 16:30:21 -0800

    Adjusted Intern.test.js to pass it's test cases

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,25 +1,24 @@
-const Employee = require('../lib/Employee');
 const Intern = require('../lib/Intern');
 
 // Test the intern object
 test('create the intern object', () => {
-    const employee = new Employee('Test', 'Intern');
-    const intern = new Intern('UCLA');
+    const intern = new Intern('Test', 'Test@example.com', 1, 'Intern', 'UCLA');
 
-    expect(employee.name).toBe('Test');
-    expect(employee.role).toBe('Intern');
+    expect(intern.name).toBe('Test');
+    expect(intern.email).toBe('Test@example.com');
+    expect(intern.id).toEqual(expect.any(Number));
+    expect(intern.role).toBe('Intern');
     expect(intern.school).toBe('UCLA');
 });
 
 // Test for getSchool() method
 test('Test for the interns school', () => {
-    const intern = new Intern();
-    intern.getSchool();
+    const intern = new Intern('Test', 'Test@example.com', 1, 'Intern', 'UCLA');
+    expect(intern.getSchool()).toEqual('UCLA');
 });
 
 // Test for getRole and return 'intern'
 test('get employee role and return the Intern position', () => {
-    const employee = new Employee('', 'Intern');
-    employee.getRole('Intern');
-    expect(employee.role).toBe('Intern');
+    const intern = new Intern('Test', 'Test@example.com', 1, 'Intern', 'UCLA');
+    expect(intern.getRole()).toEqual('Intern');
 });
