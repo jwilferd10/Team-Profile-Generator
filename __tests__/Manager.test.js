f50409eeb8d4e31c1403eaf9c0b0eaacd181bf78
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-03-03 16:38:38 -0800

    Adjusted Manager.test.js to pass it's test cases

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,25 +1,24 @@
-const Employee = require('../lib/Employee');
 const Manager = require('../lib/Manager')
 
 // Test the manager project
 test('create the manager object', () => {
-    const employee = new Employee('Test');
-    const manager = new Manager();
+    const manager = new Manager('Test', 'Test@example.com', 1, 'Manager', '2');
 
-    expect(employee.name).toBe('Test');
-    manager.getOfficeNumber();
+    expect(manager.name).toBe('Test');
+    expect(manager.email).toBe('Test@example.com');
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.role).toBe('Manager');
+    expect(manager.officeNumber).toBe('2');
 });
 
 // Check for property of officeNumber
 test('test for the managers office number', () => {
-    const manager = new Manager();
-    manager.getOfficeNumber();
+    const manager = new Manager('Test', 'Test@example.com', 1, 'Manager', '2');
+    expect(manager.getOfficeNumber()).toEqual('2');
 });
 
 // Run test for getRole and return Manager
 test('get employee role and return the Manager role', () => {
-    const employee = new Employee('Test', 'Manager');
-
-    employee.getRole('Manager');
-    expect(employee.role).toBe('Manager');
+    const manager = new Manager('Test', 'Test@example.com', 1, 'Manager', '2');
+    expect(manager.getRole()).toEqual('Manager');
 });
